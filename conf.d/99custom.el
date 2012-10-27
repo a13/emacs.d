385c2a38973760e30e8ef1b25228fc93b834fb88
dk <dk@porter>
2012-10-27 13:43:06 +0400

    installation script

---
+++ conf.d/99custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-default-style (quote ((java-mode . "java") (awk-mode . "awk") (other . "k&r"))))
+ '(calendar-week-start-day 1)
+ '(ls-lisp-emulation (quote MS-Windows))
+ '(ls-lisp-ignore-case t)
+ '(ls-lisp-verbosity nil)
+ '(safe-local-variable-values (quote ((auto-save-default))))
+ '(w3m-use-cookies t)
+ '(x-select-enable-clipboard t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(header-line ((t (:inherit mode-line-inactive :background "gray30" :foreground "grey90" :box nil))))
+ '(hl-line ((t (:background "brightblack"))))
+ '(linum ((t (:background "#E2E2E2" :foreground "#888888")))))
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CONFDIR=$HOME/.emacs.d
+if [ -e $CONFDIR ]
+then 
+    test  -L $CONFDIR || mv $CONFDIR ${CONFDIR}.backup
+fi
+ln -Tsf `pwd` $HOME/.emacs.d
+
+cd $CONFDIR/conf.d
+cat enabled | while read prio file; do
+    test "$file" && ln -sf  ../conf.avail/${file}.el ${prio}${file}.el
+done
