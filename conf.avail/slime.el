9c0ed7563007fe3b83bb5eb4fe9e0a8aca63cd05
dk <dk@porter>
2012-10-27 14:04:19 +0400

    changed require to with-feature macro

--- conf.avail/slime.el
+++ conf.avail/slime.el
@@ -1,9 +1,9 @@
-(setq inferior-lisp-program "/usr/bin/sbcl"
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
+(with-feature slime
+  ;; (set-language-environment "utf-8")
+  (setq slime-net-coding-system 'utf-8-unix)
+  (setq inferior-lisp-program "/usr/bin/sbcl"
       lisp-indent-function 'common-lisp-indent-function
       slime-complete-symbol-function 'slime-fuzzy-complete-symbol
       slime-startup-animation nil)
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-(require 'slime)
-;(set-language-environment "utf-8")
-(setq slime-net-coding-system 'utf-8-unix)
-(slime-setup '(slime-fancy))
+  (slime-setup '(slime-fancy)))
