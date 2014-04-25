10384648e977c27bcfdd08a36f8af2f84d40acfe
DK <beerandhot@gmail.com>
2014-04-25 15:03:51 +0400

    keymaps fixes

--- conf.avail/keymap.el
+++ conf.avail/keymap.el
@@ -3,9 +3,6 @@
 (global-set-key "\C-x\C-k" 'kill-region)
 ;; remap ctrl-h
 (global-set-key "\C-h" 'delete-backward-char)
-(global-set-key "\C-x\C-b" 'ibuffer-list-buffers)
-
-(define-key global-map [f13] 'toggle-input-method)
-(define-key isearch-mode-map [f13] 'isearch-toggle-input-method)
+(define-key isearch-mode-map "\C-h" 'isearch-delete-char)
 
 (define-key global-map [f9] 'menu-bar-mode)
