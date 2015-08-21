428a0e87fb98aeaab33767134bb2ea45f4189a88
DK <beerandhot@gmail.com>
2015-08-21 13:34:01 +0300

    ...

--- conf.avail/colors.el
+++ conf.avail/colors.el
@@ -1,7 +1,7 @@
 (setq default-font "Consolas-10")
-(set-default-font default-font)
+(set-frame-font default-font)
 ;; daemon specific settings
 (add-to-list 'default-frame-alist `(font . ,default-font))
 (setq initial-frame-alist default-frame-alist)
-(setq special-display-frame-alist default-frame-alist)
+(setq display-buffer-alist default-frame-alist)
 (setq custom-enabled-themes '(deeper-blue))
