d9fe56a3b39837bfb70d6ced7c474cb167780328
DK <beerandhot@gmail.com>
2017-03-13 17:07:39 +0300

    don't create conf.d symlinks

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 CONFDIR=$HOME/.emacs.d
 if [ -e $CONFDIR ]
-then 
-    test  -L $CONFDIR || mv $CONFDIR ${CONFDIR}.backup
+then
+    test -L $CONFDIR || mv $CONFDIR ${CONFDIR}.backup
 fi
 ln -Tsf `pwd` $HOME/.emacs.d
-
-cd $CONFDIR/conf.d
-
-/bin/ls -1 *.el | grep -v 99custom.el | xargs rm -fv
-
-cat enabled | while read prio file; do
-    test "$file" && ln -vsf  ../conf.avail/${file}.el ${prio}${file}.el
-done
 
 mkdir -p $CONFDIR/elpa
