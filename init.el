e34fb826b38bf2f26f7a10616df6194f932f80f6
DK <beerandhot@gmail.com>
2021-11-03 12:23:58 +0200

    comment jabber

--- init.el
+++ init.el
@@ -822,49 +822,49 @@
   :ensure t
   :defer t)
 
-(use-package jabber
-  :defer t
-  :config
-  (setq jabber-history-enabled t
-        jabber-use-global-history nil
-        fsm-debug nil)
-  :custom
-  (jabber-auto-reconnect t)
-  (jabber-chat-buffer-format "*-jc-%n-*")
-  (jabber-groupchat-buffer-format "*-jg-%n-*")
-  (jabber-chat-foreign-prompt-format "▼ [%t] %n> ")
-  (jabber-chat-local-prompt-format "▲ [%t] %n> ")
-  (jabber-muc-colorize-foreign t)
-  (jabber-muc-private-buffer-format "*-jmuc-priv-%g-%n-*")
-  (jabber-rare-time-format "%e %b %Y %H:00")
-  (jabber-resource-line-format "   %r - %s [%p]")
-  (jabber-roster-buffer "*-jroster-*")
-  (jabber-roster-line-format "%c %-17n")
-  (jabber-roster-show-bindings nil)
-  (jabber-roster-show-title nil)
-  (jabber-roster-sort-functions (quote (jabber-roster-sort-by-status jabber-roster-sort-by-displayname jabber-roster-sort-by-group)))
-  (jabber-show-offline-contacts nil)
-  (jabber-show-resources nil))
+;; (use-package jabber
+;;   :defer t
+;;   :config
+;;   (setq jabber-history-enabled t
+;;         jabber-use-global-history nil
+;;         fsm-debug nil)
+;;   :custom
+;;   (jabber-auto-reconnect t)
+;;   (jabber-chat-buffer-format "*-jc-%n-*")
+;;   (jabber-groupchat-buffer-format "*-jg-%n-*")
+;;   (jabber-chat-foreign-prompt-format "▼ [%t] %n> ")
+;;   (jabber-chat-local-prompt-format "▲ [%t] %n> ")
+;;   (jabber-muc-colorize-foreign t)
+;;   (jabber-muc-private-buffer-format "*-jmuc-priv-%g-%n-*")
+;;   (jabber-rare-time-format "%e %b %Y %H:00")
+;;   (jabber-resource-line-format "   %r - %s [%p]")
+;;   (jabber-roster-buffer "*-jroster-*")
+;;   (jabber-roster-line-format "%c %-17n")
+;;   (jabber-roster-show-bindings nil)
+;;   (jabber-roster-show-title nil)
+;;   (jabber-roster-sort-functions (quote (jabber-roster-sort-by-status jabber-roster-sort-by-displayname jabber-roster-sort-by-group)))
+;;   (jabber-show-offline-contacts nil)
+;;   (jabber-show-resources nil))
 
-(use-package jabber-otr
-  :ensure t
-  :defer t)
+;; (use-package jabber-otr
+;;   :ensure t
+;;   :defer t)
 
 ;; (use-package secrets-jabber
 ;;   :defer t
 ;;   :load t
 ;;   :after jabber)
 
-(use-package point-im
-  :defer t
-  :defines point-im-reply-id-add-plus
-  :after jabber
-  :quelpa
-  (point-im :repo "a13/point-im.el" :fetcher github :version original)
-  :custom
-  (point-im-reply-id-add-plus nil)
-  :hook
-  (jabber-chat-mode . point-im-mode))
+;; (use-package point-im
+;;   :defer t
+;;   :defines point-im-reply-id-add-plus
+;;   :after jabber
+;;   :quelpa
+;;   (point-im :repo "a13/point-im.el" :fetcher github :version original)
+;;   :custom
+;;   (point-im-reply-id-add-plus nil)
+;;   :hook
+;;   (jabber-chat-mode . point-im-mode))
 
 (use-package slack
   :ensure t
